0e6229ce0b54c160846f0dc2489f0a462b170ed2
Nick <nickm@ogmda.com>
2023-06-26 14:10:34 -0500

    back to edge while waiting for a fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'vercel'
+    preset: 'vercel-edge'
   },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
